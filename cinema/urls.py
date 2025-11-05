f62b34768143cf0ad0a677796b05ac30dd621972
misha-cw <bigun.mykhaylo@gmail.com>
2025-11-06 00:49:37 +0200

    Add Movie API views (GET, POST, PUT, DELETE)

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
+from cinema.views import (
+    movie_list_create,
+    movie_detail_update_delete,
+)
+
 
 app_name = "cinema"
 
 urlpatterns = [
-
+    path("movies/", movie_list_create, name="movie-list-create"),
+    path("movies/<int:pk>/", movie_detail_update_delete, name="movie-detail-update-delete"),
 ]
--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("cinema.urls",namespace="cinema"))
+    path("api/cinema/", include("cinema.urls", namespace="cinema"))
 ]
